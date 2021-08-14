9589b82f9645da8f50745138a58bda4a55568880
CaChiJ <53603478+CaChiJ@users.noreply.github.com>
2021-08-14 17:03:15 +0900

    분석 알고리즘 추가 및 로딩 화면 연결

---
+++ js/analyize.js
@@ -0,0 +1,89 @@
+class Analysis {
+    constructor(startIdx, prior_str, new_str, removed, abb_type) {
+        this.startIdx = startIdx;
+        this.prior_str = prior_str;
+        this.new_str = new_str;
+        this.removed = removed;
+        this.abb_type = abb_type;
+    }
+}
+
+const RESULT_PAGE = "/html/result.html";
+const LOADING_PAGE = "/html/loading.html";
+
+const REMOVED_SPACE = '_';
+
+const replaceTable = [
+    ['하여', '해', 1],
+    ['하였', '했', 1],
+    ['되어', '돼', 1],
+    ['되었', '됐', 1],
+    ['었었', '었', 1],
+    ['했었', '했', 1],
+    ['보았', '봤', 1],
+    ['주었', '줬', 1],
+    [' 매우 ', REMOVED_SPACE, 3],
+    [' 아주 ', REMOVED_SPACE, 3],
+    [' 굉장히 ', REMOVED_SPACE, 3],
+    [' 정말 ', REMOVED_SPACE, 3],
+    [' 약간 ', REMOVED_SPACE, 3],
+    [' 결코 ', REMOVED_SPACE, 3],
+    [' 조금 ', REMOVED_SPACE, 3],
+    ['다는 것', '(으)ㅁ', 3],
+    ['와 같은 ', ' 같은 ', 1],
+    ['와 같이', ' 같이 ', 1],
+    ['게 되었', '었/였/았', 3],
+    ['게 됐', '었/였/았', 2]
+];
+
+
+function sleep(ms) {
+    return new Promise((r) => setTimeout(r, ms));
+}
+
+
+function saveInput() {
+    let writingArea = document.querySelector('.input-box .writing-area');
+    localStorage.setItem("original_text", JSON.stringify(writingArea.value));
+}
+
+
+function analyize() {
+    let originalText = JSON.parse(localStorage.getItem("original_text"));
+
+    let passCount = 0;
+    let result = [];
+
+    for (let idx = 0; idx < originalText.length; ++idx) {
+        if (passCount > 0) {
+            passCount--;
+            continue;
+        }
+
+        for (let rep = 0; rep < replaceTable.length; ++rep) {
+            if (idx + replaceTable[rep].length >= originalText.length) {
+                continue;
+            }
+
+            let isSame = true;
+
+            for (let i = 0; i < replaceTable[rep][0].length; ++i) {
+                if (replaceTable[rep][0][i] != originalText[idx + i]) {
+                    isSame = false;
+                    break;
+                }
+            }
+
+            if (isSame) {
+                result.push(new Analysis(idx, replaceTable[rep][0], replaceTable[rep][1], replaceTable[rep][2]));
+                passCount = replaceTable[rep][0].length - 1;
+                break;
+            }
+        }
+    }
+
+    localStorage.setItem("analysis", JSON.stringify(result));
+    setTimeout(function() {
+        window.location.href = RESULT_PAGE;
+    }, 1000);
+}
