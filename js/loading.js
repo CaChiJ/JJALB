0bf0dde2c24157db863177d85cf6ad800216b2de
CaChiJ <53603478+CaChiJ@users.noreply.github.com>
2021-08-14 15:23:48 +0900

    로딩화면 메시지 갱신 기능 추가

---
+++ js/loading.js
@@ -0,0 +1,17 @@
+import data from '/data/loadingMsgs.js';
+
+console.log(data);
+function showLoadingMsg() {
+    const INTERVAL = 3000;
+    const tipBox = document.querySelector('.short-tip');
+    
+    changeMsg(tipBox, data);
+    setInterval(function() {changeMsg(tipBox, data)}, INTERVAL);
+}
+
+function changeMsg(msgBox, msgs) {
+    let randomNum = Math.floor(Math.random() * msgs.length);
+    msgBox.innerText = `[${msgs[randomNum].type}] ${msgs[randomNum].msg}`;
+}
+
+showLoadingMsg();
