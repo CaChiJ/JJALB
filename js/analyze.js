53ff7b410140ccd4bcb8546e8ece24fae064ee78
CaChiJ <chj7239@gmail.com>
2021-09-12 16:00:55 +0900

    <생략> 항목 선택 시 텍스트에는 공란으로 대체

--- js/analyze.js
+++ js/analyze.js
@@ -1,11 +1,11 @@
 class Analysis {
-    constructor(start_idx, prior_str, new_str, removed, abb_type) {
-        this.start_idx = start_idx;
-        this.prior_str = prior_str;
-        this.new_str = new_str;
-        this.removed = removed;
-        this.abb_type = abb_type;
-    }
+  constructor(start_idx, prior_str, new_str, removed, abb_type) {
+    this.start_idx = start_idx;
+    this.prior_str = prior_str;
+    this.new_str = new_str;
+    this.removed = removed;
+    this.abb_type = abb_type;
+  }
 }
 
 const RESULT_PAGE = "/html/result.html";
@@ -15,117 +15,121 @@ const ORIGINAL_TEXT_KEY = "original_text";
  * 리스트의 각 요소는 순서대로 [<축약 대상>, <대체할 문자>, <축약한 수>] 를 의미함.
  * 와일드카드 문자(*, ?, ~) 지원하지 않음.
  */
-const REMOVED_SPACE = '<생략>';
+const REMOVED_SPACE = "<생략>";
 const replaceTable = [
-    ['하여', '해', 1],
-    ['해서 ', '해 ', 1],
-    ['하였', '했', 1],
-    ['되어', '돼', 1],
-    ['되었', '됐', 1],
-    ['었었', '었', 1],
-    ['았었', '았', 1],
-    ['했었', '했', 1],
-    ['보았', '봤', 1],
-    ['주었', '줬', 1],
-    ['매우 ', REMOVED_SPACE, 3],
-    ['굉장히 ', REMOVED_SPACE, 3],
-    ['정말 ', REMOVED_SPACE, 3],
-    ['약간 ', REMOVED_SPACE, 3],
-    ['결코 ', REMOVED_SPACE, 3],
-    ['아주 ' , REMOVED_SPACE, 3],
-    ['조금 ', REMOVED_SPACE, 3],
-    [' 수 있게', '도록', 3],
-    ['다는 것', '(으)ㅁ', 3],
-    ['와 같은', ' 같은', 1],
-    ['와 같이', ' 같이', 1],
-    ['게 되었', '었/였/았', 3],
-    ['게 되어', '어/여/아', 3],
-    ['게 됐', '었/였/았', 2],
-    ['기 위하여서 ', '고자', 5],
-    ['기 위해서 ', '고자', 3],
-    ['기 위하여 ', '고자', 3],
-    ['기 위해 ', '고자', 2],
-    ['에 관하여서', '을(를)', 5],
-    ['에 관해서', '을(를)', 4],
-    ['에 관하여', '을(를)', 4],
-    ['에 관해', '을(를)', 3],
-    ['에 대하여서', '을(를)', 5],
-    ['에 대하여', '을(를)', 4],
-    ['에 대해서', '을(를)', 4],
-    ['에 대해', '을(를)', 3],
-    ['를 통하여서 ', '로 ', 5],
-    ['를 통해서 ', '로 ', 4],
-    ['를 통하여 ', '로 ', 4],
-    ['를 통해 ', '로 ', 3],
-    ['을 통하여서 ', '(으)로 ', 4],
-    ['을 통해서 ', '(으)로 ', 3],
-    ['을 통하여 ', '(으)로 ', 3],
-    ['을 통해 ', '(으)로 ', 2],
-    ['하기도 했', '했', 4],
-    ['되기도 했', '됐', 4],
-    ['하면서도', '해도', 2],
-    ['하면서 ', '하며 ', 1],
-    ['되면서 ', '돼며 ', 1],
-    ['수많은', '많은', 1],
-    ['이루었', '이뤘', 1],
-    ['도중', '중', 1]
+  ["하여", "해", 1],
+  ["해서 ", "해 ", 1],
+  ["하였", "했", 1],
+  ["되어", "돼", 1],
+  ["되었", "됐", 1],
+  ["었었", "었", 1],
+  ["았었", "았", 1],
+  ["했었", "했", 1],
+  ["보았", "봤", 1],
+  ["주었", "줬", 1],
+  ["매우 ", REMOVED_SPACE, 3],
+  ["굉장히 ", REMOVED_SPACE, 3],
+  ["정말 ", REMOVED_SPACE, 3],
+  ["약간 ", REMOVED_SPACE, 3],
+  ["결코 ", REMOVED_SPACE, 3],
+  ["아주 ", REMOVED_SPACE, 3],
+  ["조금 ", REMOVED_SPACE, 3],
+  [" 수 있게", "도록", 3],
+  ["다는 것", "(으)ㅁ", 3],
+  ["와 같은", " 같은", 1],
+  ["와 같이", " 같이", 1],
+  ["게 되었", "었/였/았", 3],
+  ["게 되어", "어/여/아", 3],
+  ["게 됐", "었/였/았", 2],
+  ["기 위하여서 ", "고자", 5],
+  ["기 위해서 ", "고자", 3],
+  ["기 위하여 ", "고자", 3],
+  ["기 위해 ", "고자", 2],
+  ["에 관하여서", "을(를)", 5],
+  ["에 관해서", "을(를)", 4],
+  ["에 관하여", "을(를)", 4],
+  ["에 관해", "을(를)", 3],
+  ["에 대하여서", "을(를)", 5],
+  ["에 대하여", "을(를)", 4],
+  ["에 대해서", "을(를)", 4],
+  ["에 대해", "을(를)", 3],
+  ["를 통하여서 ", "로 ", 5],
+  ["를 통해서 ", "로 ", 4],
+  ["를 통하여 ", "로 ", 4],
+  ["를 통해 ", "로 ", 3],
+  ["을 통하여서 ", "(으)로 ", 4],
+  ["을 통해서 ", "(으)로 ", 3],
+  ["을 통하여 ", "(으)로 ", 3],
+  ["을 통해 ", "(으)로 ", 2],
+  ["하기도 했", "했", 4],
+  ["되기도 했", "됐", 4],
+  ["하면서도", "해도", 2],
+  ["하면서 ", "하며 ", 1],
+  ["되면서 ", "돼며 ", 1],
+  ["수많은", "많은", 1],
+  ["이루었", "이뤘", 1],
+  ["도중", "중", 1],
 ];
 
-
 function setPercentage(percentage) {
-    const progressBox = document.querySelector('.progress');
-    progressBox.innerText = `${percentage}%`;
+  const progressBox = document.querySelector(".progress");
+  progressBox.innerText = `${percentage}%`;
 }
-
 
 // input.html submit 버튼의 onclick 함수
 function saveInput() {
-    let writingArea = document.querySelector('.input-box .writing-area');
-    localStorage.setItem(ORIGINAL_TEXT_KEY, JSON.stringify(writingArea.value));
+  let writingArea = document.querySelector(".input-box .writing-area");
+  localStorage.setItem(ORIGINAL_TEXT_KEY, JSON.stringify(writingArea.value));
 }
-
 
 // 분석 함수
 function analyze() {
-    let originalText = JSON.parse(localStorage.getItem(ORIGINAL_TEXT_KEY));   //분석 대상
+  let originalText = JSON.parse(localStorage.getItem(ORIGINAL_TEXT_KEY)); //분석 대상
 
-    let passCount = 0;
-    let result = [];
+  let passCount = 0;
+  let result = [];
 
-    // 비교를 통한 축약 분석
-    for (let idx = 0; idx < originalText.length; ++idx) {
-        setPercentage(Math.round((idx+1) / originalText.length * 100));     // 분석 진척도 표시
+  // 비교를 통한 축약 분석
+  for (let idx = 0; idx < originalText.length; ++idx) {
+    setPercentage(Math.round(((idx + 1) / originalText.length) * 100)); // 분석 진척도 표시
 
-        if (passCount > 0) {
-            passCount--;
-            continue;
-        }
-
-        for (let rep = 0; rep < replaceTable.length; ++rep) {
-            if (idx + replaceTable[rep].length >= originalText.length) {
-                continue;
-            }
-
-            let isSame = true;
-
-            for (let i = 0; i < replaceTable[rep][0].length; ++i) {
-                if (replaceTable[rep][0][i] !== originalText[idx + i]) {
-                    isSame = false;
-                    break;
-                }
-            }
-
-            if (isSame) {
-                result.push(new Analysis(idx, replaceTable[rep][0], replaceTable[rep][1], replaceTable[rep][2]));
-                passCount = replaceTable[rep][0].length - 1;
-                break;
-            }
-        }
+    if (passCount > 0) {
+      passCount--;
+      continue;
     }
 
-    localStorage.setItem("analysis", JSON.stringify(result));   // 분석 결과 저장
+    for (let rep = 0; rep < replaceTable.length; ++rep) {
+      if (idx + replaceTable[rep].length >= originalText.length) {
+        continue;
+      }
 
-    setTimeout(function() {
-        window.location.href = RESULT_PAGE;     // 결과 화면 이동
-    }, 1000);
+      let isSame = true;
+
+      for (let i = 0; i < replaceTable[rep][0].length; ++i) {
+        if (replaceTable[rep][0][i] !== originalText[idx + i]) {
+          isSame = false;
+          break;
+        }
+      }
+
+      if (isSame) {
+        result.push(
+          new Analysis(
+            idx,
+            replaceTable[rep][0],
+            replaceTable[rep][1],
+            replaceTable[rep][2]
+          )
+        );
+        passCount = replaceTable[rep][0].length - 1;
+        break;
+      }
+    }
+  }
+
+  localStorage.setItem("analysis", JSON.stringify(result)); // 분석 결과 저장
+
+  setTimeout(function () {
+    window.location.href = RESULT_PAGE; // 결과 화면 이동
+  }, 1000);
 }
--- js/result.js
+++ js/result.js
@@ -1,211 +1,243 @@
-const textMonitor = document.querySelector('.text-monitor');
-const analysisBox = document.querySelector('.suggestion-box');
-const idxMonitor = document.querySelector('.idx-monitor');
-const countMonitor = document.querySelector('.count-monitor');
+const textMonitor = document.querySelector(".text-monitor");
+const analysisBox = document.querySelector(".suggestion-box");
+const idxMonitor = document.querySelector(".idx-monitor");
+const countMonitor = document.querySelector(".count-monitor");
 
-const resetBtn = document.querySelector('.reset-btn');
-const selectAllBtn = document.querySelector('.select-all-btn');
-const copyBtn = document.querySelector('.copy-btn');
+const resetBtn = document.querySelector(".reset-btn");
+const selectAllBtn = document.querySelector(".select-all-btn");
+const copyBtn = document.querySelector(".copy-btn");
 
-const analysis = JSON.parse(localStorage.getItem('analysis'));
-const originalText = JSON.parse(localStorage.getItem('original_text'));
+const analysis = JSON.parse(localStorage.getItem("analysis"));
+const originalText = JSON.parse(localStorage.getItem("original_text"));
 
-const SELECTED_CLASSNAME = 'selected';
-const REPLACED_CLASSNAME = 'replaced';
-const MOUSEENTERTEXT_CLASSNAME = 'mouseenter-text';
+const SELECTED_CLASSNAME = "selected";
+const REPLACED_CLASSNAME = "replaced";
+const MOUSEENTERTEXT_CLASSNAME = "mouseenter-text";
 
 let currentStrLength = originalText.length;
 
+const REMOVED_SPACE_SUGGESTION = "<생략>";
+const REMOVED_SPACE_HOLDER = "";
 
 function init() {
-    setTextMonitor(originalText);
-    updateReduceCount(0);
+  setTextMonitor(originalText);
+  updateReduceCount(0);
 
-    if (analysis.length === 0) {
-        let alertEmpty = document.createElement('span');
-        alertEmpty.innerText = '수정할 부분이 없습니다.';
-        alertEmpty.classList.add('alert-empty');
-        analysisBox.appendChild(alertEmpty);
-        return;
-    }
+  if (analysis.length === 0) {
+    let alertEmpty = document.createElement("span");
+    alertEmpty.innerText = "수정할 부분이 없습니다.";
+    alertEmpty.classList.add("alert-empty");
+    analysisBox.appendChild(alertEmpty);
+    return;
+  }
 
-    for (let i = 0; i < analysis.length; ++i) {
-        let now = analysis[i];
-        const block = makeAnalysisBlock(now.start_idx, now.prior_str, now.new_str, now.removed);
-        block.id = `suggestion-${i}`;
-        block.addEventListener('click', handleClickSuggestion);
-        analysisBox.appendChild(block);
-    }
+  for (let i = 0; i < analysis.length; ++i) {
+    let now = analysis[i];
+    const block = makeAnalysisBlock(
+      now.start_idx,
+      now.prior_str,
+      now.new_str,
+      now.removed
+    );
+    block.id = `suggestion-${i}`;
+    block.addEventListener("click", handleClickSuggestion);
+    analysisBox.appendChild(block);
+  }
 
-    resetBtn.addEventListener('click', handleClickReset);
-    selectAllBtn.addEventListener('click', handleClickSelectAll);
-    copyBtn.addEventListener('click', handleClickCopy);
+  resetBtn.addEventListener("click", handleClickReset);
+  selectAllBtn.addEventListener("click", handleClickSelectAll);
+  copyBtn.addEventListener("click", handleClickCopy);
 }
-
 
 function makeAnalysisBlock(start_idx, prior_str, new_str, removed) {
-    const eBlock = document.createElement("div");
-    const eStartIdx = document.createElement("span");
-    const ePriorStr = document.createElement("span");
-    const eNewStr = document.createElement("span");
-    const eRemoved = document.createElement("span");
+  const eBlock = document.createElement("div");
+  const eStartIdx = document.createElement("span");
+  const ePriorStr = document.createElement("span");
+  const eNewStr = document.createElement("span");
+  const eRemoved = document.createElement("span");
 
-    eBlock.classList.add("suggestion");
-    eStartIdx.classList.add("sug-idx");
-    ePriorStr.classList.add("sug-prior");
-    eNewStr.classList.add("sug-new");
-    eRemoved.classList.add("sug-removed");
+  eBlock.classList.add("suggestion");
+  eStartIdx.classList.add("sug-idx");
+  ePriorStr.classList.add("sug-prior");
+  eNewStr.classList.add("sug-new");
+  eRemoved.classList.add("sug-removed");
 
-    eStartIdx.innerText = parseInt(start_idx) + 1;
-    ePriorStr.innerText = prior_str;
-    eNewStr.innerText = new_str;
-    eRemoved.innerText = removed;
+  eStartIdx.innerText = parseInt(start_idx) + 1;
+  ePriorStr.innerText = prior_str;
+  eNewStr.innerText = new_str;
+  eRemoved.innerText = removed;
 
-    eBlock.appendChild(eStartIdx);
-    eBlock.appendChild(ePriorStr);
-    eBlock.appendChild(eNewStr);
-    eBlock.appendChild(eRemoved);
+  eBlock.appendChild(eStartIdx);
+  eBlock.appendChild(ePriorStr);
+  eBlock.appendChild(eNewStr);
+  eBlock.appendChild(eRemoved);
 
-    return eBlock;
+  return eBlock;
 }
-
 
 function makeSpanStr(idx, str) {
-    let newChar = document.createElement('span');
-    newChar.innerText = str;
-    newChar.id = `text-${idx}`
-    newChar.addEventListener('mouseenter', handleMouseenterChar);
-    newChar.addEventListener('mouseleave', handleMouseleaveChar);
-    return newChar;
+  let newChar = document.createElement("span");
+  newChar.innerText = str;
+  newChar.id = `text-${idx}`;
+  newChar.addEventListener("mouseenter", handleMouseenterChar);
+  newChar.addEventListener("mouseleave", handleMouseleaveChar);
+  return newChar;
 }
-
 
 function setTextMonitor(str) {
-    while(textMonitor.firstChild) {
-        textMonitor.firstChild.remove();
-    }
+  while (textMonitor.firstChild) {
+    textMonitor.firstChild.remove();
+  }
 
-    for(let i = 0; i < str.length; ++i) {
-        textMonitor.appendChild(makeSpanStr(i, str[i]));
-    }
+  for (let i = 0; i < str.length; ++i) {
+    textMonitor.appendChild(makeSpanStr(i, str[i]));
+  }
 }
-
 
 function updateText(anaylsisNum, status) {
-    const selectedText = document.getElementById(`text-${analysis[anaylsisNum].start_idx}`);
-    let isReplaced = selectedText.classList.contains(REPLACED_CLASSNAME);
+  const selectedText = document.getElementById(
+    `text-${analysis[anaylsisNum].start_idx}`
+  );
+  let isReplaced = selectedText.classList.contains(REPLACED_CLASSNAME);
 
-    if(status && isReplaced || !status && !isReplaced) {
-        return;
+  if ((status && isReplaced) || (!status && !isReplaced)) {
+    return;
+  }
+
+  if (status) {
+    for (let i = 0; i < analysis[anaylsisNum].prior_str.length; ++i) {
+      document
+        .getElementById(`text-${analysis[anaylsisNum].start_idx + i}`)
+        .remove();
     }
 
-    if(status) {
-        for(let i = 0; i < analysis[anaylsisNum].prior_str.length; ++i) {
-            document.getElementById(`text-${analysis[anaylsisNum].start_idx + i}`).remove();
-        }
-        
-        let newStr = makeSpanStr(analysis[anaylsisNum].start_idx, analysis[anaylsisNum].new_str);
-        newStr.classList.add(REPLACED_CLASSNAME);
-        textMonitor.insertBefore(newStr, document.getElementById(`text-${analysis[anaylsisNum].start_idx + analysis[anaylsisNum].prior_str.length}`));
+    let newStr;
+    if (analysis[anaylsisNum].new_str == REMOVED_SPACE_SUGGESTION) {
+      newStr = makeSpanStr(
+        analysis[anaylsisNum].start_idx,
+        REMOVED_SPACE_HOLDER
+      );
     } else {
-        document.getElementById(`text-${analysis[anaylsisNum].start_idx}`).remove();
-
-        for(let i = 0; i < analysis[anaylsisNum].prior_str.length; ++i) {
-            let newStr = makeSpanStr(analysis[anaylsisNum].start_idx + i, analysis[anaylsisNum].prior_str[i]);
-            textMonitor.insertBefore(newStr, document.getElementById(`text-${analysis[anaylsisNum].start_idx + analysis[anaylsisNum].prior_str.length}`));
-        }
+      newStr = makeSpanStr(
+        analysis[anaylsisNum].start_idx,
+        analysis[anaylsisNum].new_str
+      );
     }
-}
 
+    newStr.classList.add(REPLACED_CLASSNAME);
+    textMonitor.insertBefore(
+      newStr,
+      document.getElementById(
+        `text-${
+          analysis[anaylsisNum].start_idx +
+          analysis[anaylsisNum].prior_str.length
+        }`
+      )
+    );
+  } else {
+    document.getElementById(`text-${analysis[anaylsisNum].start_idx}`).remove();
+
+    for (let i = 0; i < analysis[anaylsisNum].prior_str.length; ++i) {
+      let newStr = makeSpanStr(
+        analysis[anaylsisNum].start_idx + i,
+        analysis[anaylsisNum].prior_str[i]
+      );
+      textMonitor.insertBefore(
+        newStr,
+        document.getElementById(
+          `text-${
+            analysis[anaylsisNum].start_idx +
+            analysis[anaylsisNum].prior_str.length
+          }`
+        )
+      );
+    }
+  }
+}
 
 function updateReduceCount(delta) {
-    currentStrLength += delta;
-    countMonitor.innerText = `${currentStrLength}자 / ${originalText.length}자`;
+  currentStrLength += delta;
+  countMonitor.innerText = `${currentStrLength}자 / ${originalText.length}자`;
 }
-
 
 function resetReduceCount() {
-    currentStrLength = originalText.length;
-    countMonitor.innerText = `${currentStrLength}자 / ${originalText.length}자`;
+  currentStrLength = originalText.length;
+  countMonitor.innerText = `${currentStrLength}자 / ${originalText.length}자`;
 }
-
 
 function handleClickSuggestion(event) {
-    let block;  // 클릭된 suggestion 블럭
+  let block; // 클릭된 suggestion 블럭
 
-    if (event.target.classList.contains('suggestion')) {
-        block = event.target;
-    } else {
-        block = event.target.parentElement;
-    }
+  if (event.target.classList.contains("suggestion")) {
+    block = event.target;
+  } else {
+    block = event.target.parentElement;
+  }
 
-    let blockNum = block.id.substr(11);
-    document.getElementById(`text-${analysis[blockNum].start_idx}`).scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+  let blockNum = block.id.substr(11);
+  document
+    .getElementById(`text-${analysis[blockNum].start_idx}`)
+    .scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
 
-    if (block.classList.contains(SELECTED_CLASSNAME)) {
-        // 분석 해제
-        block.classList.remove(SELECTED_CLASSNAME);
-        updateText(blockNum, false);
-        updateReduceCount(analysis[blockNum].removed);
-    } else {
-        // 분석 적용
-        block.classList.add(SELECTED_CLASSNAME);
-        updateText(blockNum, true);
-        updateReduceCount(-analysis[blockNum].removed);
-    }
+  if (block.classList.contains(SELECTED_CLASSNAME)) {
+    // 분석 해제
+    block.classList.remove(SELECTED_CLASSNAME);
+    updateText(blockNum, false);
+    updateReduceCount(analysis[blockNum].removed);
+  } else {
+    // 분석 적용
+    block.classList.add(SELECTED_CLASSNAME);
+    updateText(blockNum, true);
+    updateReduceCount(-analysis[blockNum].removed);
+  }
 }
-
 
 function handleClickReset() {
-    if(confirm("현재 변경사항을 모두 초기 상태로 되돌리시겠습니까?")) {
-        setTextMonitor(originalText);
-        const suggestions = analysisBox.querySelectorAll('.suggestion');
+  if (confirm("현재 변경사항을 모두 초기 상태로 되돌리시겠습니까?")) {
+    setTextMonitor(originalText);
+    const suggestions = analysisBox.querySelectorAll(".suggestion");
 
-        for(let i = 0; i < suggestions.length; ++i) {
-            suggestions[i].classList.remove(SELECTED_CLASSNAME);
-            if(suggestions[i].classList.contains(SELECTED_CLASSNAME)) {}
-        }
-
-        resetReduceCount();
-    }
-}
-
-
-function handleClickSelectAll() {
-    let newReduceCount = 0;
-    
-    for(let i = 0; i < analysis.length; ++i) {
-        let block = document.getElementById(`suggestion-${i}`);
-        block.classList.add(SELECTED_CLASSNAME);
-        updateText(i, true);
-        newReduceCount += analysis[i].removed;
+    for (let i = 0; i < suggestions.length; ++i) {
+      suggestions[i].classList.remove(SELECTED_CLASSNAME);
+      if (suggestions[i].classList.contains(SELECTED_CLASSNAME)) {
+      }
     }
 
     resetReduceCount();
-    updateReduceCount(-newReduceCount);
+  }
 }
 
+function handleClickSelectAll() {
+  let newReduceCount = 0;
+
+  for (let i = 0; i < analysis.length; ++i) {
+    let block = document.getElementById(`suggestion-${i}`);
+    block.classList.add(SELECTED_CLASSNAME);
+    updateText(i, true);
+    newReduceCount += analysis[i].removed;
+  }
+
+  resetReduceCount();
+  updateReduceCount(-newReduceCount);
+}
 
 function handleClickCopy() {
-    let tmpText = document.createElement('textarea');
-    tmpText.value = textMonitor.innerText;
-    document.body.appendChild(tmpText);
-    tmpText.select();
-    document.execCommand('copy');
-    document.body.removeChild(tmpText);
+  let tmpText = document.createElement("textarea");
+  tmpText.value = textMonitor.innerText;
+  document.body.appendChild(tmpText);
+  tmpText.select();
+  document.execCommand("copy");
+  document.body.removeChild(tmpText);
 }
-
 
 function handleMouseenterChar(event) {
-    idxMonitor.innerText = `위치: ${parseInt(event.target.id.substr(5)) + 1}`;
-    event.target.classList.add(MOUSEENTERTEXT_CLASSNAME);
+  idxMonitor.innerText = `위치: ${parseInt(event.target.id.substr(5)) + 1}`;
+  event.target.classList.add(MOUSEENTERTEXT_CLASSNAME);
 }
-
 
 function handleMouseleaveChar(event) {
-    idxMonitor.innerText = '';
-    event.target.classList.remove(MOUSEENTERTEXT_CLASSNAME);
+  idxMonitor.innerText = "";
+  event.target.classList.remove(MOUSEENTERTEXT_CLASSNAME);
 }
-
 
 init();
